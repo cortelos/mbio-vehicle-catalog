10cf5d5471fde074f38b3951403bfb29e4548f19
Andre Macedo <andre.macedo@mercedes-benz.io>
2018-01-31 09:51:19 +0000

    Update ports and containers names.

--- catalog-frontend/webpack.production.js
+++ catalog-frontend/webpack.production.js
@@ -2,7 +2,7 @@ var webpack = require('webpack')
 
 module.exports = [
   new webpack.DefinePlugin({
-    "WEBPACK_BACKEND_URL": JSON.stringify("http://ec2-34-217-60-197.us-west-2.compute.amazonaws.com:9001/catalog/v1/")
+    "WEBPACK_BACKEND_URL": JSON.stringify("http://ec2-54-245-189-204.us-west-2.compute.amazonaws.com:9001/catalog/v1/")
   }),
   new webpack.DefinePlugin({
   'process.env': {
